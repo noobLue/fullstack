79f92a0e0cf2b60923ef400eae9cead46c41ff90
Nooblue <jontsa96@gmail.com>
2023-11-17 19:08:27 +0200

    removed secret

--- part2/countries/src/services/restweather.js
+++ part2/countries/src/services/restweather.js
@@ -1,13 +1,8 @@
 import axios from 'axios'
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
-const apiKey = "86145b6e88aa078f29aeb256704b2fcb" // process.env.WEATHER_KEY
+const apiKey = process.env.WEATHER_KEY
 
 const getCountryWeather = (lat, lon) => {
-    if(!lat)
-    {
-        console.error("Called without lat/long");
-    }
-    console.log(apiKey);
     return axios.get(`${baseUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}`)
 }
 
